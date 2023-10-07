a15ed931a85b6a665f3111f0ce1ba51f3aacfc90
gsunil1996 <gajulasunilkumar123@gmail.com>
2023-10-07 15:33:45 +0530

    update

--- backend/Server.js
+++ backend/Server.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 const cors = require("cors");
 
 const routes = require("./routes/ToDoRoute");
+const newTodoRoutes = require("./routes/NewToDoRoute");
 
 const app = express();
 const PORT = process.env.PORT;
@@ -15,14 +16,15 @@ app.use(express.json());
 app.use(cors());
 
 mongoose
-    .connect(process.env.MONGODB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => console.log("Mongodb Connected..."))
-    .catch((err) => console.error(err));
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Mongodb Connected..."))
+  .catch((err) => console.error(err));
 
 // Routes
 app.use(routes);
+app.use(newTodoRoutes);
 
 app.listen(PORT, () => console.log("Server running on port " + PORT));
--- frontend/src/redux/features/apiSlice.js
+++ frontend/src/redux/features/apiSlice.js
@@ -1,59 +1,54 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { apiSlice } from "../api/apiSlice";
 
-export const apiSlice = createApi({
-    reducerPath: 'api',
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:5000'
+export const todoSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getTodos: builder.query({
+      query: () => "/",
+      providesTags: ["Todos"],
     }),
-    tagTypes: ['Todos'],
-    endpoints: (builder) => ({
-        getTodos: builder.query({
-            query: () => '/',
-            providesTags: ['Todos']
-        }),
-        getSingleTodo: builder.query({
-            query: (id) => `/todo/${id}`,
-            providesTags: ['Todos']
-        }),
-        addTodo: builder.mutation({
-            query: (text) => ({
-                url: '/save',
-                method: 'POST',
-                body: text
-            }),
-            invalidatesTags: ['Todos']
-        }),
-        updateTodo: builder.mutation({
-            query: ({ id, ...rest }) => ({
-                url: `/update/${id}`,
-                method: 'PUT',
-                body: rest
-            }),
-            invalidatesTags: ['Todos']
-        }),
-        changeTodoStatus: builder.mutation({
-            query: ({ id, ...rest }) => ({
-                url: `/status/${id}`,
-                method: 'PUT',
-                body: rest
-            }),
-            invalidatesTags: ['Todos']
-        }),
-        deleteTodo: builder.mutation({
-            query: ({ id }) => ({
-                url: `/delete/${id}`,
-                method: 'DELETE',
-            }),
-            invalidatesTags: ['Todos']
-        }),
-    })
-})
+    getSingleTodo: builder.query({
+      query: (id) => `/todo/${id}`,
+      providesTags: ["Todos"],
+    }),
+    addTodo: builder.mutation({
+      query: (text) => ({
+        url: "/save",
+        method: "POST",
+        body: text,
+      }),
+      invalidatesTags: ["Todos"],
+    }),
+    updateTodo: builder.mutation({
+      query: ({ id, ...rest }) => ({
+        url: `/update/${id}`,
+        method: "PUT",
+        body: rest,
+      }),
+      invalidatesTags: ["Todos"],
+    }),
+    changeTodoStatus: builder.mutation({
+      query: ({ id, ...rest }) => ({
+        url: `/status/${id}`,
+        method: "PUT",
+        body: rest,
+      }),
+      invalidatesTags: ["Todos"],
+    }),
+    deleteTodo: builder.mutation({
+      query: ({ id }) => ({
+        url: `/delete/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Todos"],
+    }),
+  }),
+});
 
 export const {
-    useGetTodosQuery,
-    useGetSingleTodoQuery,
-    useAddTodoMutation,
-    useDeleteTodoMutation,
-    useUpdateTodoMutation,
-    useChangeTodoStatusMutation
-} = apiSlice
+  useGetTodosQuery,
+  useGetSingleTodoQuery,
+  useAddTodoMutation,
+  useDeleteTodoMutation,
+  useUpdateTodoMutation,
+  useChangeTodoStatusMutation,
+} = apiSlice;
